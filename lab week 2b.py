73a214b88b33ddf2619dba3ca8b467ed56d92daf
smorrison21 <morrisons@uchicago.edu>
2024-03-28 14:43:44 -0500

    finished 2

--- lab week 2b.py
+++ lab week 2b.py
@@ -26,7 +26,11 @@ while i < 10:
 #1 2
 #1 2 3
 #1 2 3 4
-    
+  
+for x in range(1, 5):
+    for y in range(1, x + 1):
+        print(y, end = " ")
+    print()  
 
 #3
 start_list = [[2, 3, 4], [6, 8, 9]]
